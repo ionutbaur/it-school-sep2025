fa047a524cfa8ee7be675f763039e70ffae9276d
ionutbaur <ionut.baur@gmail.com>
2025-09-04 09:59:33 +0300

    course 10 - wrapper classes demo.

--- src/com/itschool/session10/course/CourseMain.java
+++ src/com/itschool/session10/course/CourseMain.java
@@ -3,6 +3,32 @@ package com.itschool.session10.course;
 public class CourseMain {
 
     public static void main(String[] args) {
-        // here we will call the needed stuff to run the program
+        boolean primitiveBool = true;
+        Boolean objectBool = true;
+
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+
+        // int nullInt = null; won't work
+
+        Integer objectNumber = 2; // autoboxing : primitive 2 -> Integer obj
+
+        Integer intNumObj = Integer.valueOf(objectNumber);
+        int intNumPrimitive = intNumObj; //auto-unboxing: obj -> int primitive
+
+        Integer nullInteger = null;
+
+        System.out.println(a);
+        System.out.println(objectNumber);
+        System.out.println(nullInteger);
+
+        String objectNumberAsString = objectNumber.toString();
+        System.out.println("Object number as a String is: " + objectNumberAsString);
+
+        String numberAsString = nullInteger.toString();
+        System.out.println(numberAsString);
+
+        Byte byteObj = 1;
     }
 }
